ba105fc397dc0a68c55ff4e943435af3431481a9
Fuji <fujiji.entire1414@gmail.com>
2025-04-30 22:18:09 +0900

    fixed

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,29 @@
+local opt = vim.opt
+
+-- Set width for 2-byte characters to double
+opt.ambiwidth = 'double'
+
+-- Enable automatic indentation
+opt.autoindent = true
+-- Enable smart indentation
+opt.smartindent = true
+
+-- Set the number of spaces that a <Tab> in the file counts for
+opt.tabstop = 4
+-- Set the number of spaces that a <Tab> counts for while performing editing operations
+opt.softtabstop = 4
+
+-- Show line numbers
+opt.number = true
+
+-- Load the package manager
 require('plugin_manager')
 
+-- Load plugins
+require('plugins')
+
+
+-- Apply plugin settings
+require('plugins.tokyonight')
+require('plugins.nvim-tree')
+require('plugins.nvim-web-devicons')
---
+++ .config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,11 @@
+return {
+    "nvim-tree/nvim-web-devicons",
+
+    event = "BufReadPre", 
+    config = function() 
+        require("nvim-web-devicons").setup {
+        default = true,
+
+    }
+end,
+}
