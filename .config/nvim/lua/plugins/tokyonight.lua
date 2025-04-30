423e9a72b5e429f0bbfc7fb12e4d8789fca490a6
Fuji <fujiji.entire1414@gmail.com>
2025-04-30 22:13:56 +0900

    fixed color

---
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+require("tokyonight").setup({
+    style = "night",
+    transparent = true,
+    styles = {
+        comments = { italic = true },
+        keywords = { italic = true }
+    }
+})
+
+vim.cmd('colorscheme tokyonight')
+vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
