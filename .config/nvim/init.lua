2dd4595fcf885be5fef6bafc2e5870ffa7f35912
Fuji <fujiji.entire1414@gmail.com>
2025-05-01 18:28:39 +0900

    fixed

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,6 @@
-local opt = vim.opt
+-- vim.g.loaded_netrw       = 1
+-- vim.g.loaded_netrwPlugin = 1
 
--- Set width for 2-byte characters to double
-opt.ambiwidth = 'double'
-
--- Enable automatic indentation
-opt.autoindent = true
--- Enable smart indentation
-opt.smartindent = true
-
--- Set the number of spaces that a <Tab> in the file counts for
-opt.tabstop = 4
--- Set the number of spaces that a <Tab> counts for while performing editing operations
-opt.softtabstop = 4
-
--- Show line numbers
-opt.number = true
-
--- Load the package manager
-require('plugin_manager')
-
--- Load plugins
-require('plugins')
-
-
--- Apply plugin settings
-require('plugins.tokyonight')
-require('plugins.nvim-tree')
-require('plugins.nvim-web-devicons')
+vim.loader.enable()
+require 'options'
+require 'extensions'
