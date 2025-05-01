f7a76851fa845287d91703ebf06f2a6383bbca27
Fuji <fujiji.entire1414@gmail.com>
2025-05-01 18:26:49 +0900

    fixed

--- .config/nvim/lua/extentions/nvim-tree.lua
+++ .config/nvim/lua/extentions/nvim-tree.lua
@@ -1,15 +1,42 @@
 return {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = { "NvimTreeToggle", "NvimTreeFindFile", "NvimTreeRefresh" },
+    keys = {
+      { "<C-n>",    "<cmd>NvimTreeToggle<CR>",    desc = "Toggle NvimTree" },
+      { "<leader>r","<cmd>NvimTreeRefresh<CR>",   desc = "Refresh NvimTree" },
+      { "<leader>n","<cmd>NvimTreeFindFile<CR>",  desc = "Find File in NvimTree" },
+    },
     config = function()
       require("nvim-tree").setup {
         sort_by = "name",
-        view = { width = 30, side = "left" },
-        renderer = { highlight_git = true, icons = { show = { git = true } } },
-        filters = { dotfiles = false },
-    }
-        vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { silent = true, desc = "Toggle NvimTree" })
-        vim.keymap.set("n", "<leader>r", ":NvimTreeRefresh<CR>", { silent = true, desc = "Refresh NvimTree" })
-        vim.keymap.set("n", "<leader>n", ":NvimTreeFindFile<CR>", { silent = true, desc = "Find File in NvimTree" })
+        view = {
+          width = 30,
+          side  = "left",
+        },
+        renderer = {
+          highlight_git = true,
+          icons = {
+            show = {
+              git    = true,
+              folder = true,
+              file   = true,
+            },
+          },
+        },
+        filters = {
+          dotfiles = false,
+        },
+        update_focused_file = {
+          enable     = true,
+          update_cwd = true,
+        },
+        git = {
+          enable = true,
+          ignore = true,
+        },
+      }
     end,
-}  
+  }
+  
+  
