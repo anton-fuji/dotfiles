c1e16841a293ac2776a511dff9df81ebe7f483d5
Fuji <fujiji.entire1414@gmail.com>
2025-08-03 22:09:10 +0900

    [fixed] add icon

--- nvim/lua/extensions/alpha.lua
+++ nvim/lua/extensions/alpha.lua
@@ -27,15 +27,17 @@ return {
         [[                                                                       ]],
       }
   
-      -- cusomize
-      dashboard.section.buttons.val = {
-        dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-        dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-        dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
-        dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
-      }
+    -- cusomize
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+      dashboard.button("t", "󰺯  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+      dashboard.button("l", "󰒲  Lazy Plugin Manager", ":Lazy<CR>"),
+      dashboard.button("q", "󰈆  Quit Neovim", ":qa<CR>"),
+    }
+
   
       -- Footer option
       local function footer()
