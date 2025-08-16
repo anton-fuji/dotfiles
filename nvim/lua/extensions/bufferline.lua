bbcfff7e75cfe9752495bac3b70e4c32ea43b820
Fuji <fujiji.entire1414@gmail.com>
2025-08-16 11:52:14 +0900

    modify:terminalのbufferを自動で削除

--- nvim/lua/extensions/bufferline.lua
+++ nvim/lua/extensions/bufferline.lua
@@ -1,21 +1,29 @@
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    keys = {
-      { "<Tab>",      "<Cmd>BufferLineCycleNext<CR>", desc = "Next Tab" },
-      { "<S-Tab>",    "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Tab" },
-      { "<leader>bd", "<Cmd>bdelete<CR>",             desc = "close current bufferline" },
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
+  keys = {
+    { "<Tab>",      "<Cmd>BufferLineCycleNext<CR>", desc = "Next Tab" },
+    { "<S-Tab>",    "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Tab" },
+    { "<leader>bd", "<Cmd>bdelete<CR>",             desc = "close current bufferline" },
+  },
+  opts = {
+    options = {
+      mode = "buffers",
+      separator_style = "slope",
+      hover = { enabled = true, delay = 200 },
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      modified_icon = "●",
+      diagnostics = "nvim_lsp",
+      diagnostics_indicator = function(count, level, is_visible)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
+      end,
+      custom_filter = function(buf_number)
+        local buf_name = vim.api.nvim_buf_get_name(buf_number)
+        return buf_name ~= "" and vim.bo[buf_number].buftype ~= "terminal"
+      end,
     },
-    opts = {
-      options = {
-        mode = "buffers",
-        separator_style = "slope",
-        hover = { enabled = true, delay = 200 },
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        modified_icon = "●",
-      },
-    },
+  },
 }
-  
