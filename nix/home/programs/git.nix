d7896fe6eebc9b90de42dbeb15a4bc7b4772be31
Fuji <fujiji.entire1414@gmail.com>
2025-12-21 15:14:21 +0900

    fix settings section

--- nix/home/programs/git.nix
+++ nix/home/programs/git.nix
@@ -3,28 +3,19 @@
 {
   programs.git = {
     enable = true;
-    userName = "Fuji";
-    userEmail = "164746053+anton-fuji@users.noreply.github.com";
 
-    extraConfig = {
+    settings = {
+      user = {
+        name = "Fuji";
+        email = "164746053+anton-fuji@users.noreply.github.com";
+      };
+
       init.defaultBranch = "main";
       pull.rebase = false;
       core.editor = "nvim";
     };
-
-    # delta (diff viewer)
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        line-numbers = true;
-        side-by-side = true;
-      };
-    };
-
   };
 
-  # GitHub CLI
   programs.gh = {
     enable = true;
     settings = {
@@ -32,5 +23,15 @@
       editor = "nvim";
     };
   };
-}
 
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+
+      options = {
+        navigate = true;
+        "line-numbers" = true;
+        "side-by-side" = true;
+      };
+  };
+}
