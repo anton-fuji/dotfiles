852e8688c7c349da19b291a07954626affeafefd
Fuji <fujiji.entire1414@gmail.com>
2026-01-26 10:37:30 +0900

    resize window

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,6 +1,10 @@
 -- Normal to Command (コマンド入力をより使いやすく)
-vim.keymap.set("n", ":", ";")
-vim.keymap.set("n", ";", ":")
+-- vim.keymap.set("n", ":", ";")
+-- vim.keymap.set("n", ";", ":")
+
+-- 絶対行・相対行表示
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- ペースト後にカーソルを移動しない
 vim.keymap.set("v", "y", "y`]")
@@ -12,11 +16,10 @@ vim.keymap.set("n", "X", '"_X')
 vim.keymap.set("n", "s", '"_s')
 
 vim.keymap.set("i", "jj", "<ESC>:<C-u>w<CR>")
-vim.keymap.set({ "i", "c" }, "<M-k>", "\\", { noremap = true, silent = true })
+vim.keymap.set({ 'i', 'c' }, '<M-k>', '\\', { noremap = true, silent = true })
 
--- カーソル移動
-vim.keymap.set("n", "gh", "^")
-vim.keymap.set("n", "gl", "$")
+-- スペース
+vim.keymap.set("i", "<S-Space>", "    ", { noremap = true, silent = true, desc = 'Insert 4 spaces' })
 
 -- leader設定
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
@@ -24,8 +27,19 @@ vim.g.mapleader      = " "
 vim.g.maplocalleader = "\\"
 
 -- 選択系
-vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true })
-vim.keymap.set('v', '<C-a>', '<Esc>ggVG', { noremap = true, silent = true })
+-- vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true })
+-- vim.keymap.set('v', '<C-a>', '<Esc>ggVG', { noremap = true, silent = true })
+
+-- スプリット (非leader系)
+-- vim.keymap.set("n", "<S-j>", ":split<CR>", { silent = true })
+-- vim.keymap.set("n", "<S-l>", ":vsplit<CR>", { silent = true })
+
+-- ウィンドウ移動
+vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
+
 
 -- スクロール
 vim.keymap.set("n", "<C-j>", "20j", { noremap = true, silent = true })
@@ -37,10 +51,37 @@ vim.keymap.set("n", "tt", function()
 end, { silent = true })
 
 -- 下部にterminalを表示
+-- vim.keymap.set("n", "te", function()
+--   vim.cmd("belowright split | terminal")
+--   vim.cmd("resize 20")
+-- end, { silent = true })
+
 vim.keymap.set("n", "te", function()
-  vim.cmd("belowright split | terminal")
-  vim.cmd("resize 15")
-end, { silent = true })
+  -- 既に開いていたら閉じる・閉じてたら開く
+  vim.cmd("ToggleTerm direction=horizontal")
+end, { silent = true, desc = "ToggleTerm: 横分割ターミナル" })
+
 
 -- <Esc> Normalモード
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { silent = true })
+
+-- Oil
+vim.keymap.set("n", "<leader>-", "<CMD>Oil .<CR>", { desc = "Open cwd" })
+
+-- Window Resize
+vim.keymap.set("n", "<C-n>", function()
+  print("Resize: h/l (ESC to exit)")
+  while true do
+    local key = vim.fn.getchar()
+    local char = type(key) == "number" and vim.fn.nr2char(key) or key
+
+    if char == "h" then
+      vim.cmd("vertical resize -3")
+    elseif char == "l" then
+      vim.cmd("vertical resize +3")
+    elseif key == 27 then -- ESC
+      print("")
+      break
+    end
+  end
+end, { desc = "Resize mode" })
