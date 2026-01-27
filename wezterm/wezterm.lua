9622967c276e0cb682c04b2af3c24b1ee336c16e
Fuji <fujiji.entire1414@gmail.com>
2026-01-27 17:15:01 +0900

    setup CopyMode

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local background = require("background")
 local transparency = require("transparency")
 
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.automatically_reload_config = true
 config.font_size = 13.7
@@ -84,12 +85,56 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 end)
 
 ----------------------------------------------------
--- keybinds
+-- Keybindings & CopyMode
 ----------------------------------------------------
 config.disable_default_key_bindings = true
-config.keys = require("keybinds").keys
-config.key_tables = require("keybinds").key_tables
-table.insert(config.keys, transparency.key)
 config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
+
+local keys = require("keybinds").keys or {}
+local key_tables = require("keybinds").key_tables or {}
+
+table.insert(keys, {
+  key = 'X',
+  mods = 'CTRL|SHIFT',
+  action = act.ActivateCopyMode,
+})
+table.insert(keys, transparency.key)
+
+-- CopyMode
+key_tables.copy_mode = {
+  { key = 'Escape', mods = 'NONE',  action = act.CopyMode 'Close' },
+  { key = 'q',      mods = 'NONE',  action = act.CopyMode 'Close' },
+
+  { key = 'h',      mods = 'NONE',  action = act.CopyMode 'MoveLeft' },
+  { key = 'j',      mods = 'NONE',  action = act.CopyMode 'MoveDown' },
+  { key = 'k',      mods = 'NONE',  action = act.CopyMode 'MoveUp' },
+  { key = 'l',      mods = 'NONE',  action = act.CopyMode 'MoveRight' },
+  { key = 'w',      mods = 'NONE',  action = act.CopyMode 'MoveForwardWord' },
+  { key = 'b',      mods = 'NONE',  action = act.CopyMode 'MoveBackwardWord' },
+  { key = 'e',      mods = 'NONE',  action = act.CopyMode 'MoveForwardWordEnd' },
+  { key = '0',      mods = 'NONE',  action = act.CopyMode 'MoveToStartOfLine' },
+  { key = '$',      mods = 'NONE',  action = act.CopyMode 'MoveToEndOfLineContent' },
+  { key = '^',      mods = 'NONE',  action = act.CopyMode 'MoveToStartOfLineContent' },
+  { key = 'g',      mods = 'NONE',  action = act.CopyMode 'MoveToScrollbackTop' },
+  { key = 'G',      mods = 'SHIFT', action = act.CopyMode 'MoveToScrollbackBottom' },
+  { key = 'd',      mods = 'CTRL',  action = act.CopyMode { MoveByPage = 0.5 } },
+  { key = 'u',      mods = 'CTRL',  action = act.CopyMode { MoveByPage = -0.5 } },
+  { key = 'v',      mods = 'NONE',  action = act.CopyMode { SetSelectionMode = 'Cell' } },
+  { key = 'V',      mods = 'NONE',  action = act.CopyMode { SetSelectionMode = 'Line' } },
+  { key = 'v',      mods = 'CTRL',  action = act.CopyMode { SetSelectionMode = 'Block' } },
+
+  {
+    key = 'y',
+    mods = 'NONE',
+    action = act.Multiple {
+      { CopyTo = 'ClipboardAndPrimarySelection' },
+      { CopyMode = 'Close' },
+    }
+  },
+  { key = 'Enter', mods = 'NONE', action = act.CopyMode 'ClearSelectionMode' },
+}
+
+config.keys = keys
+config.key_tables = key_tables
 
 return config
