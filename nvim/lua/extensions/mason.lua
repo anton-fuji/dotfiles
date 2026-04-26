96db83a9aa0f06c7244de346114ad67f996fb69b
Fuji <fujiji.entire1414@gmail.com>
2026-04-26 18:55:40 +0900

    rust-analyzer

--- nvim/lua/extensions/mason.lua
+++ nvim/lua/extensions/mason.lua
@@ -1,28 +1,30 @@
 require("mason").setup({
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗",
-      },
-      border = "rounded",
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
     },
-    max_concurrent_installers = 4,
-    log_level = vim.log.levels.INFO,
-  })
-  
-  
-  require("mason-lspconfig").setup({
-    ensure_installed = {
-      "ts_ls",
-      "lua_ls",
-      "gopls",
-      "pyright",
-      "jsonls",
-      "yamlls",
-      "dockerls",
-      "marksman",
-      "ltex",
-    },
-    automatic_installation = true,
+    border = "rounded",
+  },
+  max_concurrent_installers = 4,
+  log_level = vim.log.levels.INFO,
+})
+
+
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "rust-analyzer",
+    "ts_ls",
+    "lua_ls",
+    "gopls",
+    "pyright",
+    "jsonls",
+    "yamlls",
+    "dockerls",
+    "marksman",
+    "ltex",
+  },
+  automatic_installation = true,
 })
