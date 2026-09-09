d4aaf483c66de21d2472653ee2e69a4739e6a22e
Fuji <fujiji.entire1414@gmail.com>
2026-09-09 22:31:19 +0900

    fix(lazygit): 設定の二重管理を解消

--- nix/home/programs/lazygit/default.nix
+++ nix/home/programs/lazygit/default.nix
@@ -1,18 +1,7 @@
-{ pkgs, lib, ... }:
+{ ... }:
 
-let
-  lazygitConfigYaml = builtins.readFile ./config.yml;
-
-  lazygitConfigWithDeltaPath =
-    builtins.replaceStrings [ "pager: delta " ] [ "pager: ${lib.getExe pkgs.delta}" ]
-      lazygitConfigYaml;
-in
 {
   programs.lazygit = {
     enable = true;
-  };
-
-  xdg.configFile."lazygit/config.yml" = {
-    text = lazygitConfigWithDeltaPath;
   };
 }
